7bedaa834efc6ce34a30ca26c7fc538204becdd4
Derian-Web <id1474416@outlook.com>
2020-07-19 21:01:31 -0500

    add peticiones

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import axios from 'axios'
 import './App.css';
 import Form from './components/form'
 class App extends React.Component {
@@ -10,22 +11,36 @@ class App extends React.Component {
       usuarios: []
     }
   }
-  componentDidMount() {
-        fetch('https://academlo-api-users.herokuapp.com/users')
-          .then((response) => {
-            return response.json()
-          })
-          .then((result) => {
-           this.setState({ usuarios: result.data })
-        })
+  deletUsers = async (id) => {
+    const url = 'https://academlo-api-users.herokuapp.com/user/2'
+    await axios.delete(url)
+    alert('se borro exitosamente')
+
+  }
+   async componentDidMount() {
+    const url = 'https://academlo-api-users.herokuapp.com/users';
+    const rest = await axios.get(url);
+    console.log(rest)
+    this.setState({usuarios: rest.data.data})
     }
 
   render(){
     return (
       <div className="App">
         <h1>Api</h1>
-        <p>{this.state.usuarios.name}</p>
         <Form />
+        <button onClick={this.deletUsers}>Borra usuario</button>
+        <div className="col-md-8">
+                    <ul className="list-group">
+                        {
+                            this.state.usuarios.map(user => (
+                                <li className="list-group-item list-group-item-action" key={user.id}>
+                                    {user.name}
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </div>
       </div>
     );
   }
